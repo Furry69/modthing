b70c42ff9ad56a89f79ed612956097b25be7bbe3
Furry42069 <user@DESKTOP-8UV51IE.fios-router.home>
2021-07-29 02:37:38 -0400

    ok

--- src/main/java/net/mcreator/featuretest/procedures/TurkeyEntityFallsProcedure.java
+++ src/main/java/net/mcreator/featuretest/procedures/TurkeyEntityFallsProcedure.java
@@ -1,11 +1,19 @@
 package net.mcreator.featuretest.procedures;
 
+import net.minecraft.potion.Effects;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.Entity;
+
+import net.mcreator.featuretest.FeatureTest01ModElements;
+import net.mcreator.featuretest.FeatureTest01Mod;
+
+import java.util.Map;
+
 @FeatureTest01ModElements.ModElement.Tag
 public class TurkeyEntityFallsProcedure extends FeatureTest01ModElements.ModElement {
-
 	public TurkeyEntityFallsProcedure(FeatureTest01ModElements instance) {
 		super(instance, 42);
-
 	}
 
 	public static void executeProcedure(Map<String, Object> dependencies) {
@@ -14,12 +22,8 @@ public class TurkeyEntityFallsProcedure extends FeatureTest01ModElements.ModElem
 				FeatureTest01Mod.LOGGER.warn("Failed to load dependency entity for procedure TurkeyEntityFalls!");
 			return;
 		}
-
 		Entity entity = (Entity) dependencies.get("entity");
-
 		if (entity instanceof LivingEntity)
 			((LivingEntity) entity).addPotionEffect(new EffectInstance(Effects.SLOW_FALLING, (int) 60, (int) 1, (false), (false)));
-
 	}
-
 }
