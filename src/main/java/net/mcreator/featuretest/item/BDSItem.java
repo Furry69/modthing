795dde03f0bf961bf814097f9b131842d665b3be
Furry42069 <user@DESKTOP-8UV51IE.fios-router.home>
2021-07-29 00:14:20 -0400

    GAY

--- src/main/java/net/mcreator/featuretest/item/BDSItem.java
+++ src/main/java/net/mcreator/featuretest/item/BDSItem.java
@@ -45,7 +45,7 @@ public class BDSItem extends FeatureTest01ModElements.ModElement {
 			public Ingredient getRepairMaterial() {
 				return Ingredient.EMPTY;
 			}
-		}, 3, -1.5f, new Item.Properties().group(ItemGroup.COMBAT)) {
+		}, 3, -3.4f, new Item.Properties().group(ItemGroup.COMBAT)) {
 		}.setRegistryName("bds"));
 	}
 }
