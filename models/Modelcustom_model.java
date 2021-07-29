ea2d384dadea11ff2d7b1b67c68022dad7936152
Furry42069 <user@DESKTOP-8UV51IE.fios-router.home>
2021-07-29 19:52:17 -0400

    wolf

---
+++ models/Modelcustom_model.java
@@ -0,0 +1,97 @@
+// Made with Blockbench 3.9.2
+// Exported for Minecraft version 1.15 - 1.16 with MCP mappings
+// Paste this class into your mod and generate all required imports
+
+public static class Modelcustom_model extends EntityModel<Entity> {
+	private final ModelRenderer head;
+	private final ModelRenderer body;
+	private final ModelRenderer body_rotation;
+	private final ModelRenderer mane;
+	private final ModelRenderer mane_rotation;
+	private final ModelRenderer leg1;
+	private final ModelRenderer leg2;
+	private final ModelRenderer leg3;
+	private final ModelRenderer leg4;
+	private final ModelRenderer tail;
+	private final ModelRenderer tail_r1;
+
+	public Modelcustom_model() {
+		textureWidth = 64;
+		textureHeight = 32;
+
+		head = new ModelRenderer(this);
+		head.setRotationPoint(1.0F, 13.5F, -7.0F);
+		head.setTextureOffset(0, 0).addBox(-4.0F, -3.0F, -2.0F, 6.0F, 6.0F, 4.0F, 0.0F, false);
+		head.setTextureOffset(16, 14).addBox(0.0F, -5.0F, 0.0F, 2.0F, 2.0F, 1.0F, 0.0F, false);
+		head.setTextureOffset(16, 14).addBox(-4.0F, -5.0F, 0.0F, 2.0F, 2.0F, 1.0F, 0.0F, false);
+		head.setTextureOffset(0, 10).addBox(-2.5F, -0.02F, -5.0F, 3.0F, 3.0F, 4.0F, 0.0F, false);
+
+		body = new ModelRenderer(this);
+		body.setRotationPoint(0.0F, 14.0F, 2.0F);
+
+		body_rotation = new ModelRenderer(this);
+		body_rotation.setRotationPoint(0.0F, 0.0F, 0.0F);
+		body.addChild(body_rotation);
+		setRotationAngle(body_rotation, 1.5708F, 0.0F, 0.0F);
+		body_rotation.setTextureOffset(18, 14).addBox(-3.0F, -2.0F, -3.0F, 6.0F, 9.0F, 6.0F, 0.0F, false);
+
+		mane = new ModelRenderer(this);
+		mane.setRotationPoint(1.0F, 14.0F, 2.0F);
+
+		mane_rotation = new ModelRenderer(this);
+		mane_rotation.setRotationPoint(-1.0F, 2.5F, -2.5F);
+		mane.addChild(mane_rotation);
+		setRotationAngle(mane_rotation, 1.5708F, 0.0F, 0.0F);
+		mane_rotation.setTextureOffset(21, 0).addBox(-4.0F, -5.5F, -0.5F, 8.0F, 6.0F, 7.0F, 0.0F, false);
+
+		leg1 = new ModelRenderer(this);
+		leg1.setRotationPoint(2.5F, 16.0F, 7.0F);
+		leg1.setTextureOffset(0, 18).addBox(-5.0F, 0.0F, -1.0F, 2.0F, 8.0F, 2.0F, 0.0F, false);
+
+		leg2 = new ModelRenderer(this);
+		leg2.setRotationPoint(-0.5F, 16.0F, 7.0F);
+		leg2.setTextureOffset(0, 18).addBox(1.0F, 0.0F, -1.0F, 2.0F, 8.0F, 2.0F, 0.0F, false);
+
+		leg3 = new ModelRenderer(this);
+		leg3.setRotationPoint(2.5F, 16.0F, -4.0F);
+		leg3.setTextureOffset(0, 18).addBox(-5.0F, 0.0F, -1.0F, 2.0F, 8.0F, 2.0F, 0.0F, false);
+
+		leg4 = new ModelRenderer(this);
+		leg4.setRotationPoint(-0.5F, 16.0F, -4.0F);
+		leg4.setTextureOffset(0, 18).addBox(1.0F, 0.0F, -1.0F, 2.0F, 8.0F, 2.0F, 0.0F, false);
+
+		tail = new ModelRenderer(this);
+		tail.setRotationPoint(1.0F, 12.0F, 10.0F);
+
+		tail_r1 = new ModelRenderer(this);
+		tail_r1.setRotationPoint(-1.0F, 12.0F, -10.0F);
+		tail.addChild(tail_r1);
+		setRotationAngle(tail_r1, 0.3927F, 0.0F, 0.0F);
+		tail_r1.setTextureOffset(9, 18).addBox(-1.0F, -8.0F, 11.0F, 2.0F, 7.0F, 2.0F, 0.0F, false);
+	}
+
+	@Override
+	public void setRotationAngles(Entity entity, float limbSwing, float limbSwingAmount, float ageInTicks,
+			float netHeadYaw, float headPitch) {
+		// previously the render function, render code was moved to a method below
+	}
+
+	@Override
+	public void render(MatrixStack matrixStack, IVertexBuilder buffer, int packedLight, int packedOverlay, float red,
+			float green, float blue, float alpha) {
+		head.render(matrixStack, buffer, packedLight, packedOverlay);
+		body.render(matrixStack, buffer, packedLight, packedOverlay);
+		mane.render(matrixStack, buffer, packedLight, packedOverlay);
+		leg1.render(matrixStack, buffer, packedLight, packedOverlay);
+		leg2.render(matrixStack, buffer, packedLight, packedOverlay);
+		leg3.render(matrixStack, buffer, packedLight, packedOverlay);
+		leg4.render(matrixStack, buffer, packedLight, packedOverlay);
+		tail.render(matrixStack, buffer, packedLight, packedOverlay);
+	}
+
+	public void setRotationAngle(ModelRenderer modelRenderer, float x, float y, float z) {
+		modelRenderer.rotateAngleX = x;
+		modelRenderer.rotateAngleY = y;
+		modelRenderer.rotateAngleZ = z;
+	}
+}
---
+++ src/main/java/net/mcreator/featuretest/procedures/HellHoundRightClickedOnEntityProcedure.java
@@ -0,0 +1,112 @@
+package net.mcreator.featuretest.procedures;
+
+import net.minecraft.world.server.ServerWorld;
+import net.minecraft.world.World;
+import net.minecraft.world.IWorld;
+import net.minecraft.world.GameType;
+import net.minecraft.particles.ParticleTypes;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.item.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.passive.TameableEntity;
+import net.minecraft.entity.SpawnReason;
+import net.minecraft.entity.MobEntity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.ILivingEntityData;
+import net.minecraft.entity.Entity;
+import net.minecraft.client.network.play.NetworkPlayerInfo;
+import net.minecraft.client.entity.player.AbstractClientPlayerEntity;
+import net.minecraft.client.Minecraft;
+
+import net.mcreator.featuretest.entity.HellHoundEntity;
+import net.mcreator.featuretest.FeatureTest01ModElements;
+import net.mcreator.featuretest.FeatureTest01Mod;
+
+import java.util.Map;
+
+@FeatureTest01ModElements.ModElement.Tag
+public class HellHoundRightClickedOnEntityProcedure extends FeatureTest01ModElements.ModElement {
+	public HellHoundRightClickedOnEntityProcedure(FeatureTest01ModElements instance) {
+		super(instance, 71);
+	}
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("entity") == null) {
+			if (!dependencies.containsKey("entity"))
+				FeatureTest01Mod.LOGGER.warn("Failed to load dependency entity for procedure HellHoundRightClickedOnEntity!");
+			return;
+		}
+		if (dependencies.get("sourceentity") == null) {
+			if (!dependencies.containsKey("sourceentity"))
+				FeatureTest01Mod.LOGGER.warn("Failed to load dependency sourceentity for procedure HellHoundRightClickedOnEntity!");
+			return;
+		}
+		if (dependencies.get("x") == null) {
+			if (!dependencies.containsKey("x"))
+				FeatureTest01Mod.LOGGER.warn("Failed to load dependency x for procedure HellHoundRightClickedOnEntity!");
+			return;
+		}
+		if (dependencies.get("y") == null) {
+			if (!dependencies.containsKey("y"))
+				FeatureTest01Mod.LOGGER.warn("Failed to load dependency y for procedure HellHoundRightClickedOnEntity!");
+			return;
+		}
+		if (dependencies.get("z") == null) {
+			if (!dependencies.containsKey("z"))
+				FeatureTest01Mod.LOGGER.warn("Failed to load dependency z for procedure HellHoundRightClickedOnEntity!");
+			return;
+		}
+		if (dependencies.get("world") == null) {
+			if (!dependencies.containsKey("world"))
+				FeatureTest01Mod.LOGGER.warn("Failed to load dependency world for procedure HellHoundRightClickedOnEntity!");
+			return;
+		}
+		Entity entity = (Entity) dependencies.get("entity");
+		Entity sourceentity = (Entity) dependencies.get("sourceentity");
+		double x = dependencies.get("x") instanceof Integer ? (int) dependencies.get("x") : (double) dependencies.get("x");
+		double y = dependencies.get("y") instanceof Integer ? (int) dependencies.get("y") : (double) dependencies.get("y");
+		double z = dependencies.get("z") instanceof Integer ? (int) dependencies.get("z") : (double) dependencies.get("z");
+		IWorld world = (IWorld) dependencies.get("world");
+		if ((((entity instanceof LivingEntity) ? ((LivingEntity) entity).getHeldItemMainhand() : ItemStack.EMPTY)
+				.getItem() == new ItemStack(Items.FIRE_CHARGE, (int) (1)).getItem())) {
+			if (!entity.world.isRemote())
+				entity.remove();
+			if (world instanceof ServerWorld) {
+				Entity entityToSpawn = new HellHoundEntity.CustomEntity(HellHoundEntity.entity, (World) world);
+				entityToSpawn.setLocationAndAngles(x, y, z, (float) 0, (float) 0);
+				entityToSpawn.setRenderYawOffset((float) 0);
+				if (entityToSpawn instanceof MobEntity)
+					((MobEntity) entityToSpawn).onInitialSpawn((ServerWorld) world, world.getDifficultyForLocation(entityToSpawn.getPosition()),
+							SpawnReason.MOB_SUMMONED, (ILivingEntityData) null, (CompoundNBT) null);
+				world.addEntity(entityToSpawn);
+			}
+		}
+		if ((new Object() {
+			public boolean checkGamemode(Entity _ent) {
+				if (_ent instanceof ServerPlayerEntity) {
+					return ((ServerPlayerEntity) _ent).interactionManager.getGameType() == GameType.SURVIVAL;
+				} else if (_ent instanceof PlayerEntity && _ent.world.isRemote()) {
+					NetworkPlayerInfo _npi = Minecraft.getInstance().getConnection()
+							.getPlayerInfo(((AbstractClientPlayerEntity) _ent).getGameProfile().getId());
+					return _npi != null && _npi.getGameType() == GameType.SURVIVAL;
+				}
+				return false;
+			}
+		}.checkGamemode(entity))) {
+			if (entity instanceof PlayerEntity) {
+				ItemStack _stktoremove = new ItemStack(Items.FIRE_CHARGE, (int) (1));
+				((PlayerEntity) entity).inventory.func_234564_a_(p -> _stktoremove.getItem() == p.getItem(), (int) 1,
+						((PlayerEntity) entity).container.func_234641_j_());
+			}
+			if (world instanceof ServerWorld) {
+				((ServerWorld) world).spawnParticle(ParticleTypes.HEART, x, y, z, (int) 5, 3, 3, 3, 1);
+			}
+			if ((entity instanceof TameableEntity) && (sourceentity instanceof PlayerEntity)) {
+				((TameableEntity) entity).setTamed(true);
+				((TameableEntity) entity).setTamedBy((PlayerEntity) sourceentity);
+			}
+		}
+	}
+}
