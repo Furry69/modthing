f7c69160ae2f291c45f44ffc753ce804ee302ffd
Furry42069 <user@DESKTOP-8UV51IE.fios-router.home>
2021-07-29 16:40:17 -0400

    trree

---
+++ src/main/java/net/mcreator/featuretest/block/BloodWoodLeavesBlock.java
@@ -0,0 +1,58 @@
+
+package net.mcreator.featuretest.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.world.IBlockReader;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.Direction;
+import net.minecraft.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.LeavesBlock;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.featuretest.FeatureTest01ModElements;
+
+import java.util.List;
+import java.util.Collections;
+
+@FeatureTest01ModElements.ModElement.Tag
+public class BloodWoodLeavesBlock extends FeatureTest01ModElements.ModElement {
+	@ObjectHolder("feature_test_01:blood_wood_leaves")
+	public static final Block block = null;
+	public BloodWoodLeavesBlock(FeatureTest01ModElements instance) {
+		super(instance, 56);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items
+				.add(() -> new BlockItem(block, new Item.Properties().group(ItemGroup.BUILDING_BLOCKS)).setRegistryName(block.getRegistryName()));
+	}
+	public static class CustomBlock extends LeavesBlock {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.LEAVES).sound(SoundType.PLANT).hardnessAndResistance(0.2f, 0.2f).setLightLevel(s -> 0).notSolid());
+			setRegistryName("blood_wood_leaves");
+		}
+
+		@Override
+		public int getFlammability(BlockState state, IBlockReader world, BlockPos pos, Direction face) {
+			return 30;
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(this, 1));
+		}
+	}
+}
